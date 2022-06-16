1eebead0130afb57eba577228314f681b5884399
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-16 21:12:32 +0300

    commit tree cc8bb9b220216890607bbb3ee2f6c9f55e696bf3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email){
+    const  fetchVariable = fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+    })
+    .then((res)=>res.json())
+    .then((data)=> document.body.innerHTML=data.id)
+    .catch((error)=>document.body.innerHTML=error.message)
+    return fetchVariable
+    
+}
+submitData()
